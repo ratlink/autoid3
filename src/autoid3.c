3c0b561bfedab4d1ac7e471eee83e6715a02625d
Gregory J. Gauvin <ratdock@gmail.com>
2014-06-18 21:52:00 -0700

    Main

--- src/autoid3.c
+++ src/autoid3.c
@@ -1,3 +1,13 @@
+/***************************************************************/
+/***Update media files with metadata retreived from gracenote***/
+/*****************Written By Gregory J. Gauvin******************/
+/***************************************************************/
+
 #include <stdio.h>
 #include <id3/tag.h>
+
+void main()
+{
+	
+}
 
